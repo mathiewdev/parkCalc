38034f44320f1ebfd12dfdd2469390d4f3ec042e
Mathiew Daniels-Diehl <102009467+mathiewdev@users.noreply.github.com>
2022-10-24 11:59:31 -0600

    Update edgeCases.cy.js

--- cypress/e2e/myOwnTests/edgeCases.cy.js
+++ cypress/e2e/myOwnTests/edgeCases.cy.js
@@ -12,7 +12,11 @@ describe ('Edge Case Tests', () => {
                 - manual entry
                 - non removal of existing letter values in date field
         - improper date and time formats should throw error
+            - both fields with default text and formatted date after it
             - no '/' between mmddyyyy, time not including ':', time not in proper 24/12 hr format (not sure which one)
+            - one field with proper date and other with improper date should throw error
+                - default text still in field and date added onto it 
+                -
         - leave before entry date should throw error
         - entry and leave fields are exactly the same should ____
         - 
