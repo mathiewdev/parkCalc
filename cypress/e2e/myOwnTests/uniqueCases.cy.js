b6c3419b2e7fce42b4973f794940df95c43fe7f4
mathiewdev <102009467+mathiewdev@users.noreply.github.com>
2022-10-25 00:30:25 -0600

    unique cases update

--- cypress/e2e/myOwnTests/uniqueCases.cy.js
+++ cypress/e2e/myOwnTests/uniqueCases.cy.js
@@ -24,6 +24,87 @@ describe ('Unacceptable input data', () => {
         cy.visit('https://www.shino.de/parkcalc/')
     })
 
-    it('')
+    it('didnt remove default MM/DD/YYYY from both date fields', () => {
+        cy.get('[id=StartingDate]')
+            .type('02/11/2000')
+        cy.get('[id=StartingTime]')
+            .clear()
+            .type('1:00')
+        cy.get('[name=StartingTimeAMPM][value=AM]')
+            .check()    
+        cy.get('[id=LeavingDate]')
+            .type('02/11/2000')
+        cy.get('[id=LeavingTime]')
+            .clear()
+            .type('1:30')
+        cy.get('[name=LeavingTimeAMPM][value=AM]')
+            .check()
+        cy.get('[type = submit]')
+            .click()
+        //fails to get because it doesn't exist, ie test fails
+        cy.get('td[class=SubHead]>b')
+            .should('contain', 'ERROR!')
+
+    })
+
+    it('didnt remove default MM/DD/YYYY from entry date field, improper format in entry', () => {
+        cy.get('[id=StartingDate]')
+            .type('02112000')
+        cy.get('[id=StartingTime]')
+            .clear()
+            .type('1:00')
+        cy.get('[name=StartingTimeAMPM][value=AM]')
+            .check()    
+        cy.get('[id=LeavingDate]')
+            .clear()
+            .type('02/11/2000')
+        cy.get('[id=LeavingTime]')
+            .clear()
+            .type('1:30')
+        cy.get('[name=LeavingTimeAMPM][value=AM]')
+            .check()
+        cy.get('[type = submit]')
+            .click()
+        //fails to get because it doesn't exist, ie test fails
+        cy.get('td[class=SubHead]>b')
+            .should('contain', 'ERROR!')
+
+    })
+
+})
+
+
+
+
+describe.only('Improper format', () => {
+    
+    beforeEach(() => {
+        cy.visit('https://www.shino.de/parkcalc/')
+    })
+
+    it ('no : in time field', () => {
+        //either error or 30 minute calculation, 30 hours
+        cy.get('select[id=ParkingLot]').select('Valet')
+        cy.get('[id=StartingDate]')
+            .clear()
+            .type('02112000')
+        cy.get('[id=StartingTime]')
+            .clear()
+            .type('100')
+        cy.get('[name=StartingTimeAMPM][value=AM]')
+            .check()    
+        cy.get('[id=LeavingDate]')
+            .clear()
+            .type('02112000')
+        cy.get('[id=LeavingTime]')
+            .clear()
+            .type('130')
+        cy.get('[name=LeavingTimeAMPM][value=AM]')
+            .check()
+        cy.get('[type = submit]')
+            .click()
+        cy.get('td[class=SubHead]>b')
+            .should('contain', 'ERROR!')
+    })
 
 })
